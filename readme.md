bb1ce2993bd4739ecbb1cb560e76014556227b55
wahidari <wahiid.ari@gmail.com>
2022-03-05 17:51:29 +0700

    tes

--- readme.md
+++ readme.md
@@ -1,21 +1,10 @@
-# [Nextra](https://nextra.vercel.app)
+# [Next-Docs](https://next-docss.vercel.app)
 
-_Warning: this project is not production ready, API might change without notice._
+[![](https://vercel.com/button)](https://vercel.com/import/git?s=https%3A%2F%2Fgithub.com%2Fwahidari%2Fnext-docs&c=1)
 
-[![](https://vercel.com/button)](https://vercel.com/import/git?s=https%3A%2F%2Fgithub.com%2Fshuding%2Fnextra&c=1)
-
-**Nextra** is a [Next.js](https://nextjs.org) and [MDX](https://mdxjs.com) powered, no-code site generator.
+Powered by **Nextra.** a [Next.js](https://nextjs.org) and [MDX](https://mdxjs.com) powered, no-code site generator.
 
 ![](/public/demo.png)
-
-## Development
-
-The code of Nextra is under the [`core`](https://github.com/shuding/nextra/tree/core) branch.
-
-## Themes
-
-- [nextra-theme-docs](https://github.com/vercel/swr-site) ([demo](https://swr.vercel.app))
-- [nextra-theme-blog](https://github.com/shuding/site) ([demo](https://shud.in))
 
 ---
 
--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  github: 'https://github.com/wahidari/next-docs',
+  docsRepositoryBase: 'https://github.com/wahidari/next-docs/blob/main',
+  titleSuffix: ' – Next Docs',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">Next Docs</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        Next.js Docs
       </span>
     </>
   ),
@@ -16,44 +16,22 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="Next Docs" />
+      <meta name="og:description" content="Next Docs" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-icon-180x180.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="192x192"
-        href="/android-icon-192x192.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="96x96"
-        href="/favicon-96x96.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
+      <meta name="twitter:image" content="https://next-docss.vercel.app/og.png" />
+      <meta name="twitter:site:domain" content="next-docss.vercel.app" />
+      <meta name="twitter:url" content="https://next-docss.vercel.app" />
+      <meta name="og:title" content="Next Docs" />
+      <meta name="og:image" content="https://next-docss.vercel.app/og.png" />
+      <meta name="apple-mobile-web-app-title" content="Next Docs" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
+      <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      <link rel="icon" href="/favicon.ico" />
       <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
     </>
   ),
@@ -62,6 +40,5 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
-  unstable_faviconGlyph: '👋',
+  footerText: <>MIT {new Date().getFullYear()} © Next Docs.</>
 }
