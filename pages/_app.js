3e879cc0896b97cce5e4b74b6bdf29b71867bb69
wahidari <wahiid.ari@gmail.com>
2022-03-08 20:34:43 +0700

    add tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
+import "../styles.css";
 import 'nextra-theme-docs/style.css'
 
-export default function Nextra({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
