29955199c405f7b0fe93feb0838b63636f0f6fa3
wahidari <wahiid.ari@gmail.com>
2022-03-09 19:36:10 +0700

    update

--- components/Badge.js
+++ components/Badge.js
@@ -2,23 +2,23 @@ export default function Badge({ variant, children }) {
 
   return (
     variant == "success" ?
-      <span className="text-xs font-semibold mr-2 px-2 py-1 rounded bg-green-200 text-green-600">
+      <span className="text-sm font-semibold mr-2 px-2 py-1 rounded bg-green-200 text-green-600">
         {children}
       </span>
     : variant == "danger" ?
-      <span className="text-xs font-semibold mr-2 px-2 py-1 rounded bg-red-200 text-red-600">
+      <span className="text-sm font-semibold mr-2 px-2 py-1 rounded bg-red-200 text-red-600">
         {children}
       </span>
     : variant == "warning" ?
-      <span className="text-xs font-semibold mr-2 px-2 py-1 rounded bg-yellow-200 text-yellow-600">
+      <span className="text-sm font-semibold mr-2 px-2 py-1 rounded bg-yellow-200 text-yellow-600">
         {children}
       </span>
     : variant == "dark" ?
-      <span className="text-xs font-semibold mr-2 px-2 py-1 rounded bg-gray-200 text-gray-700">
+      <span className="text-sm font-semibold mr-2 px-2 py-1 rounded bg-gray-200 text-gray-700">
         {children}
       </span>
     : 
-      <span className="text-xs font-semibold mr-2 px-2 py-1 rounded bg-blue-200 text-blue-600">
+      <span className="text-sm font-semibold mr-2 px-2 py-1 rounded bg-blue-200 text-blue-600">
         {children}
       </span>
   )
--- components/BadgeOutline.js
+++ components/BadgeOutline.js
@@ -2,23 +2,23 @@ export default function BadgeOutline({ variant, children }) {
 
   return (
     variant == "success" ?
-      <span className="text-xs font-semibold mr-2 px-2 py-1 rounded text-green-500 dark:text-green-800 border border-green-500 dark:border-green-500">
+      <span className="text-sm font-semibold mr-2 px-2 py-1 rounded text-green-500 dark:text-green-800 border border-green-500 dark:border-green-500">
         {children}
       </span>
     : variant == "danger" ?
-      <span className="text-xs font-semibold mr-2 px-2 py-1 rounded text-red-500 dark:text-red-800 border border-red-500 dark:border-red-500">
+      <span className="text-sm font-semibold mr-2 px-2 py-1 rounded text-red-500 dark:text-red-800 border border-red-500 dark:border-red-500">
         {children}
       </span>
     : variant == "warning" ?
-      <span className="text-xs font-semibold mr-2 px-2 py-1 rounded text-yellow-500 dark:text-yellow-800 border border-yellow-500 dark:border-yellow-500">
+      <span className="text-sm font-semibold mr-2 px-2 py-1 rounded text-yellow-500 dark:text-yellow-800 border border-yellow-500 dark:border-yellow-500">
         {children}
       </span>
     : variant == "dark" ?
-      <span className="text-xs font-semibold mr-2 px-2 py-1 rounded text-gray-600 dark:text-gray-600 border border-gray-500 dark:border-gray-500">
+            <span className="text-sm font-semibold mr-2 px-2 py-1 rounded text-gray-700 dark:text-gray-100 border border-gray-500 dark:border-gray-500">
         {children}
       </span>
     : 
-      <span className="text-xs font-semibold mr-2 px-2 py-1 rounded text-blue-500 dark:text-blue-800 border border-blue-500 dark:border-blue-500">
+      <span className="text-sm font-semibold mr-2 px-2 py-1 rounded text-blue-500 dark:text-blue-800 border border-blue-500 dark:border-blue-500">
         {children}
       </span>
   )
