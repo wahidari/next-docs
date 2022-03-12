6a939b34da05ebd8205d9e419f4fa4a92290c41e
wahidari <wahiid.ari@gmail.com>
2022-03-12 16:29:45 +0700

    add darkMode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 module.exports = {
     content: [
         "./components/**/*.js",
+        "./components/*.js",
         "./pages/**/*.md",
         "./pages/**/*.mdx",
         "./theme.config.js",
         "./styles.css",
         "./styles/globals.css",
     ],
+    darkMode: "class"
 };
